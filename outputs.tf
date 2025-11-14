3f526cf4b626f82512aee2920e3a608f76c36ce3
Michael Sokolowski <soko@ibm.com>
2025-11-13 22:09:01 -0600

    reset

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_id" {
-  description = "The name of the created S3 bucket"
-  value       = aws_s3_bucket.this.id
-}
-
-output "lambda_name" {
-  description = "The name of the writer lambda"
-  value       = aws_lambda_function.writer.function_name
-}
